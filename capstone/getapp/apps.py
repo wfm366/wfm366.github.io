f291328be773d35b847aef706e1bafa1c53fed88
wfm366 <102126081+wfm366@users.noreply.github.com>
2022-09-30 08:47:18 -0700

    Delete capstone directory

--- capstone/getapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GetappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'getapp'
--- capstone/getapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Input
-from django import forms
-
-
-class InputForm(forms.ModelForm):
-    class Meta:
-        model = Input
-        fields ='__all__'
